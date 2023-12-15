ff3ab0d5048775282672d6dca21394b82ce6f227
Jason Kulak <JasonKulak@yahoo.com>
2023-12-14 21:59:22 -0600

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const e = require("express")
 const express = require("express")
 const app = express()
 
@@ -17,6 +18,15 @@ app.get("/tip/:total/:tipPercentage", (req, res) => {
     const total = req.params.total
     const tipPercentage = req.params.tipPercentage
     res.send(`<h1>${tipPercentage}</h1>`)
+})
+
+app.get("/magic/:question", (req, res) => {
+
+    const question = req.params.question
+    const eightBallResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    const randomResponse = eightBallResponses[Math.floor(Math.random()*eightBallResponses.length)]
+
+    res.send(`<h1>${question}: ${randomResponse}</h1>`)
 })
 
 
